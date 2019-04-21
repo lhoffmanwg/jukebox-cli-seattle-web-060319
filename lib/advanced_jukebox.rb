53c92c787f95f29401a05a04943b887bf502bc29
Laura Hoffman <lhoffmanwg@gmail.com>
2019-04-21 23:51:08 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -1,20 +1,25 @@
+require 'pry'
+
 #Here is the song hash you will be working with. Each key is a song name and each value is the location of it's mp3 file.
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
 
-# my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
-# }
+ my_songs = {
+  "Go Go GO" => '..audio/Emerald-Park/01.mp3',
+  "LiberTeens" => '..audio/Emerald-Park/02.mp3',
+  "Hamburg" =>  '..audio/Emerald-Park/03.mp3',
+  "Guiding Light" => '..audio/Emerald-Park/04.mp3',
+  "Wolf" => '..audio/Emerald-Park/06.mp3',
+  "Graduation Failed" => '..audio/Emerald-Park/07.mp3'
+  }
 
 def help
   #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands:
+   - help : displays this help message
+   - list : displays a list of songs you can play
+   - play : lets you choose a song to play
+   - exit : exits this program"
 end
 
 
@@ -22,6 +27,9 @@ end
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
   #list the songs by name
+  my_songs do |song, path|
+    binding.pry
+  end  
 end
 
 
@@ -40,6 +48,24 @@ def exit_jukebox
   #this method is the same as in jukebox.rb
 end
 
-def run(my_songs)
+def run(songs)
   #this method is the same as in jukebox.rb
+  help
+  input = ""
+  while input != "exit"
+    puts "Please enter a command:"
+    input = gets.chomp
+    case input
+    when "play"
+      play(songs)
+    when "list"
+      list(songs)
+    when "help"
+      help
+    when "exit"
+      exit_jukebox    
+    else
+      puts "Invalid input, please try again"
+    end
+  end
 end
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +12,41 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  #this method should be the same as in jukebox.rb
+  puts "I accept the following commands:
+   - help : displays this help message
+   - list : displays a list of songs you can play
+   - play : lets you choose a song to play
+   - exit : exits this program"
+end
+
+def list(my_songs)
+  #this method is different! Collect the keys of the my_songs hash and 
+  #list the songs by name
+   my_songs do |song, path|
+    binding.pry
+  end  
+end
+
+def run(songs)
+  #this method is the same as in jukebox.rb
+  help
+  input = ""
+  while input != "exit"
+    puts "Please enter a command:"
+    input = gets.chomp
+    case input
+    when "play"
+      play(songs)
+    when "list"
+      list(songs)
+    when "help"
+      help
+    when "exit"
+      exit_jukebox    
+    else
+      puts "Invalid input, please try again"
+    end
+  end
+end
